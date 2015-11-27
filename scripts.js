4edbc68c22fdf6717370d744668ffea45023deb1
Alex Wilson <awilson@findthebest.com>
2015-11-26 23:20:43 -0800

    Updates to images structure, style and copy

--- scripts.js
+++ scripts.js
@@ -11,6 +11,42 @@
 				setTimeout(function(){
 					$whiteMask.remove();
 				}, 1000);
+			// }, 0);
 			}, 2000);
 		// });
+
+	$('.left-nav').on('click', '.nav-link', function(){
+		var hash = this.getAttribute('data-id');
+		renderPage(hash);
+		location.hash = hash;
+	});
+
+	$(window).on('hashchange', renderPageFromHash);
+
+	var imageMap = {
+		'gifts': 'laughing.jpg',
+		'love-story': 'whisper.jpg'
+	};
+
+	function renderPage(page) {
+		$('.nav-content').removeClass('active')
+			.filter('[data-id="'+page+'"]').addClass('active');
+
+		$('.normal-background').css('background-image', imageMap[page] ? 'url("images/'+imageMap[page]+'")' : '');
+	}
+
+	function renderPageFromHash() {
+		renderPage(location.hash.replace('#', ''));
+	}
+
+	// Autoload all images so they load supper duper smooth
+	setTimeout(function(){
+		var imageKeys = Object.keys(imageMap);
+		for (var i = 0, key; (key = imageKeys[i]); ++i) {
+			$('<img src="images/'+imageMap[key]+'" style="display: none;">').appendTo('body');
+		}
+	}, 500);
+
+	renderPageFromHash();
+
 })();
