6623bbd35c5b223c44247bba85bb35adc1c8607f
Alex Wilson <awilson@findthebest.com>
2015-12-18 15:34:18 -0800

    Adding mobile nav buttons

--- scripts.js
+++ scripts.js
@@ -15,15 +15,43 @@
 			// }, 2000);
 		// });
 
-	$('.left-nav').on('click', '.nav-link', function(){
+	function changePageFromDataId() {
 		var hash = this.getAttribute('data-id');
 		renderPage(hash);
 		location.hash = hash;
-	});
+	}
+
+	$('.left-nav').on('click', '.nav-link', changePageFromDataId);
 
 	$('.menu').on('click', function(){
 		$('body').toggleClass('body-show-menu');
-	})
+	});
+
+	$('.mobile-next-nav').on('click', '.mobile-next-btn', changePageFromDataId);
+
+
+	function updateMobileNavLinks() {
+		var $activeLink = $('.nav-content.active'),
+			$navLinks = $('.nav-link'),
+			$activeNavLink = $navLinks.filter('[data-id="'+$activeLink.data('id')+'"]'),
+			navLinkIndex = $navLinks.index($activeNavLink);
+
+		$('.banner-text').html(($activeNavLink.html() || 'welcome'));
+
+		if (navLinkIndex > 0) {
+			$('.mobile-next-btn[data-dir="back"]')
+				.html($navLinks.eq(navLinkIndex - 1).html())
+				.parent().removeClass('hide-mobile-nav').end()[0]
+				.setAttribute('data-id', $navLinks.eq(navLinkIndex - 1).data('id'));
+		} else {
+			$('.mobile-next-btn[data-dir="back"]').text('').parent().addClass('hide-mobile-nav');
+		}
+		if (navLinkIndex < $navLinks.length) {
+			$('.mobile-next-btn[data-dir="forward"]')
+				.html($navLinks.eq(navLinkIndex + 1).html())[0]
+				.setAttribute('data-id', $navLinks.eq(navLinkIndex + 1).data('id'));
+		}
+	}
 
 	$(window).on('hashchange', renderPageFromHash);
 
@@ -33,12 +61,14 @@
 	};
 
 	function renderPage(page) {
-		$('.nav-content').removeClass('active')
+		$('.nav-content, .nav-link').removeClass('active')
 			.filter('[data-id="'+page+'"]').addClass('active');
 
 		$('.normal-background').css('background-image', imageMap[page] ? 'url("images/'+imageMap[page]+'")' : '');
 
 		$('body').removeClass('body-show-menu');
+
+		updateMobileNavLinks();
 	}
 
 	function renderPageFromHash() {
